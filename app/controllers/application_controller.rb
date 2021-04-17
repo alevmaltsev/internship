1faec3a651cd38b02af4a56414b6b3be5519695d
Anatoliy Pronin <piton.pronin@gmail.com>
2021-04-17 11:50:06 +0400

    skip check token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   include AuthHelper
   helper_method :student_signed_in?
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 end
